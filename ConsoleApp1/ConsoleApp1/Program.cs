476a0624f376358376ba017703022d4e87e205fd
andreimarica00 <123andreimarica321@gmail.com>
2020-07-07 17:19:44 +0300

    Added add support

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Ceva Foarte Jmek");
+            Console.WriteLine("Adunare");
+            
         }
     }
 }
